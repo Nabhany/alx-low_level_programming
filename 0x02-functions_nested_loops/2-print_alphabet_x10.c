65995d08103caf246f2be9edd3f16349beaad71e
Nabhany <maamunb4@gmail.com>
2022-03-15 13:16:36 -0700

    Alphabetics

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
 }
