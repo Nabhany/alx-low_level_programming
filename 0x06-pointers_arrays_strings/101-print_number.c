a8e733b619f594e3ac1d8eb0a1a0be92676c9115
Nabhany <maamunb4@gmail.com>
2022-03-25 13:43:50 -0700

    Print number and checks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,46 +1,52 @@
 #include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * base10 - power in 10 base
+ * @n: an exponent
+ * Return: returns 10 to power exponent
+ */
+int base10(int n)
+{
+	int base = 10;
+
+	while (n > 0)
+	{
+		base *= 10;
+		n--;
+	}
+	return (base);
+}
+
+/**
+ * print_number - prints integers enters as parameters using putchar
+ * @n: integer to print
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	int power;
 
-	b = 10;
+	power = base10(8);
 
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
 		_putchar('-');
-		_putchar (n + '0');
+		n *= -1;
 	}
+
+	if (n == 0)
+		_putchar('0');
 
 	else
 	{
-		if (n < 0)
+		while (n / power == 0)
+			power /= 10;
+
+		while (power >= 1)
 		{
-			n = n * -1;
-			_putchar ('-');
+			_putchar((n / power) + '0');
+			n %= power;
+			power /= 10;
 		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
 	}
 }
